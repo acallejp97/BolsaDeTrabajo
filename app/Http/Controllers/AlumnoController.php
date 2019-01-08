feec244b4080b03ed00fcbc818796e20d481f0bc
acallejp97 <acallejp@gmail.com>
2019-01-08 09:57:36 +0100

    Varios cambios

--- app/Http/Controllers/AlumnoController.php
+++ app/Http/Controllers/AlumnoController.php
@@ -2,80 +2,42 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
+use App\Model\Alumno;
 
 //Importando las classes de modelo y servicios
-use App\Usuario;
-use App\Services\UsuarioService;
-use App\Grado;
-use App\Services\GradoService;
-use App\Curriculum;
-use App\Services\CurriculumService;
-use App\Model\Alumno;
 
 class AlumnoController extends Controller
 {
 
     public function __construct()
     {
-        $this->middleware('auth',['only'=>['aniadirUsuario' , 'aniadirCurriculum' , 'aniadirGrado']]);
+        //$this->middleware('auth', ['only' => ['aniadirUsuario', 'aniadirCurriculum', 'aniadirGrado']]);
     }
-public function VerAlumno($id){
+    public function VerAlumno($id)
+    {
 //return view ("VerAnimales");
 
 //Meto en la variable alumno toda la info con la variable que meta en rutas
-$alumno =Alumno::where('id','=',$id)->first(); 
-if(!$alumno){
-    return view ("welcome");
-}
+        $alumno = Alumno::where('id', '=', $id)->first();
+        if (!$alumno) {
+            return view("alumnos/ofertas");
+        }
 
-
-return view ("alumnos/home",compact('alumno'));
-}
-public function VerAlumnos(){
-    //return view ("VerAlumnos");
-    
-    //Saco toda la tabla de alumnos
-    // $alumnos =DB::table('alumnos')->get();   //el otro sistema
-    $alumnos =Alumno::all(); //metodo eloquent
-    if(!$alumnos){
-        return view ("welcome");
+        return view("alumnos/ofertas", compact('alumno'));
     }
-    
-    //return view ("VerAlumnos");
-    $alumno =new Alumno(); 
-    $alumno->nombre='Benito';
-$alumno->color='marron';
-$alumno->anios=1;
-$alumno->raza='buldog';
-$alumno->duenio_id=2;
-$alumnoService=new AlumnoService();
-$alumnoService->Create($alumno);
+    public function VerAlumnos()
+    {
+        //return view ("VerAlumnos");
+
+        //Saco toda la tabla de alumnos
+        // $alumnos =DB::table('alumnos')->get();   //el otro sistema
+        $alumnos = Alumno::all(); //metodo eloquent
+        if (!$alumnos) {
+            return view("alumnos/ofertas");
+        }
 
 //llamo a la funcion ver animales y regreso a la vista VerAnimales
-return $this->VerAlumnos();
+        return $this->VerAlumnos();
     }
 }
-/*
-    public function irAniadirAlumno(){
-
-    return view('aniadirAlumno');
-        }
-        public function aniadirAlumno(){
-            //return view ("VerAlumnos");
-            Log::info('aniadirAlumno');
-        $alumno =new Duenio(); 
-        $alumno->nombre='hola';
-        $alumno->usuario='fs';
-        $alumno->contrasenia=123456;
-        Log::info('aniadirAlumno');
-        $alumnoService=new AlumnoService();
-        $alumnoService->Create($alumno);
-        
-        //llamo a la funcion ver animales y regreso a la vista VerAnimales
-     
-            }
-}
-*/
+ 
---
+++ resources/views/alumnos/ofertas.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.alumno')
+
+@section('content')
+@foreach($ofertas as $oferta)
+
+<div class="row">
+        <div class="col-md-7">
+          <a href="#">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+          </a>
+        </div>
+        <div class="col-md-5">
+          <h3>$oferta->$titulo</h3>
+          <p>$oferta->$descripcion</p>
+          <p>$oferta->$puestos-vacantes</p>
+          <a class="btn btn-primary" href="#">Inscribirse</a>
+        </div>
+      </div>
+      @endforeach
+@endsection
