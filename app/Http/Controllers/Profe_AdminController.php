9f7a4fcdb12065161497362397668ce85170624d
acallejp97 <acallejp@gmail.com>
2019-01-16 08:11:45 +0100

    Perfil

--- app/Http/Controllers/Profe_AdminController.php
+++ app/Http/Controllers/Profe_AdminController.php
@@ -7,6 +7,7 @@ use App\Model\Departamento;
 use App\Model\Empresa;
 use App\Model\Grado;
 use App\Model\Oferta;
+use App\Model\Profe_Admin;
 use App\User;
 use Auth;
 
@@ -59,9 +60,17 @@ class Profe_AdminController extends Controller
 
     public function Perfil()
     {
-        $numeroInscripciones=DB::where('status','=','1')->count();
+        if (Auth::user()->rango == 1) {
+            $id_depar = Profe_Admin::select('id_depar')->where('id_user', Auth::user()->id)->get();
+            foreach ($id_depar as $id) {
+                $nombreDepar = Departamento::select('nombre')->where('id', $id->id_depar)->get();
+            }
+            return view("profesores/perfil")->with('nombreDepar', $nombreDepar);
 
-        return view("profesores/perfil");
+        } else {
+            return view("profesores/perfil");
+        }
+
     }
 
     public function Contacto()
---
+++ database/seeds/AlumnoGradoSeeder.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'vendor/fzaninotto/faker/src/autoload.php';
+
+use Illuminate\Database\Seeder;
+
+class AlumnoGradoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        DB::table('alumno_oferta')->delete();
+
+        for ($cantidadOfertas = 0; $cantidadOfertas != 20; $cantidadOfertas++) {
+            DB::table('alumno_oferta')->insert(array(
+                'id_alumno' => $faker->numberBetween(1, 25),
+                'id_oferta' => $faker->numberBetween(1, 20),
+            ));
+
+        }
+    }
+}
