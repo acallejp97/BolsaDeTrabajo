b33b71d5d1e9de719c26bb55d35271a8daca0327
acallejp97 <acallejp@gmail.com>
2019-02-01 08:54:13 +0100

    inicio Vue

--- resources/js/components/passwords.vue
+++ resources/js/components/passwords.vue
@@ -1,5 +1,39 @@
  <template>
-<div>
-</div>
+  <div id="passwords">
+    <div class="form-group">
+      <div class="col-xs-11">
+        <label for="password">
+          <h4>Password</h4>
+        </label>
+        <input
+          type="password"
+          class="form-control"
+          name="password1"
+          id="password1"
+          v-model="password1"
+          v-validate="'required'"
+          placeholder="Contraseña"
+          title="Introduce la contraseña."
+        >
+      </div>
+    </div>
 
- </template>
+    <div class="form-group">
+      <div class="col-xs-11">
+        <label for="password2">
+          <h4>Confirmar Contraseña</h4>
+        </label>
+        <input
+          type="password"
+          class="form-control"
+          name="password2"
+          id="password2"
+          v-model="password2"
+          v-validate="'required|confirmed:password'"
+          placeholder="Repite Contraseña"
+          title="Confirma tu contraseña por favor."
+        >
+      </div>
+    </div>
+  </div>
+</template>
